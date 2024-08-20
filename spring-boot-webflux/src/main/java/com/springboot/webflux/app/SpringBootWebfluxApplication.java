42a035eefe0bd212942e2b1a8170d726e04e4082
Carlos Alfonso Chong Zapata <112518890+carloschong10@users.noreply.github.com>
2024-08-20 01:09:14 -0500

    Importando datos de ejemplo y el operador flatMap

--- spring-boot-webflux/src/main/java/com/springboot/webflux/app/SpringBootWebfluxApplication.java
+++ spring-boot-webflux/src/main/java/com/springboot/webflux/app/SpringBootWebfluxApplication.java
@@ -11,8 +11,30 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import reactor.core.publisher.Flux;
 
 @SpringBootApplication
-public class SpringBootWebfluxApplication{
+public class SpringBootWebfluxApplication implements CommandLineRunner {
+
+    //    @Autowired
+    private final ProductoDao productoDao;
+
+    private static final Logger log = LoggerFactory.getLogger(SpringBootWebfluxApplication.class);
+
+    public SpringBootWebfluxApplication(ProductoDao productoDao) {
+        this.productoDao = productoDao;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(SpringBootWebfluxApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Flux.just(
+                        new Producto("Producto 1", 456.789),
+                        new Producto("Producto 2", 123D),
+                        new Producto("Producto 3", 100D),
+                        new Producto("Producto 4", 199.99),
+                        new Producto("Producto 5", 350.49)
+                ).flatMap(productoDao::save)
+                .subscribe(producto -> log.info("Insertado: " + producto.getId() + " - " + producto.getNombre()));
     }
 }
