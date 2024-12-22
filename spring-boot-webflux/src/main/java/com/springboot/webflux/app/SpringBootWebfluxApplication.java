0f0501029b61cad89c3b713d31d2716671440c02
Carlos Alfonso Chong Zapata <112518890+carloschong10@users.noreply.github.com>
2024-12-22 14:48:38 -0500

    51. Añadiendo categorías de ejemplo y el operador thenMany

--- spring-boot-webflux/src/main/java/com/springboot/webflux/app/SpringBootWebfluxApplication.java
+++ spring-boot-webflux/src/main/java/com/springboot/webflux/app/SpringBootWebfluxApplication.java
@@ -1,7 +1,11 @@
 package com.springboot.webflux.app;
 
+import com.springboot.webflux.app.dao.CategoriaDao;
 import com.springboot.webflux.app.dao.ProductoDao;
+import com.springboot.webflux.app.models.Categoria;
 import com.springboot.webflux.app.models.Producto;
+import com.springboot.webflux.app.services.ProductoService;
+import com.springboot.webflux.app.services.ProductoServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +14,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.Date;
 
@@ -17,13 +22,13 @@ import java.util.Date;
 public class SpringBootWebfluxApplication implements CommandLineRunner {
 
     //    @Autowired
-    private final ProductoDao productoDao;
+    private final ProductoServiceImpl productoService;
     private final ReactiveMongoTemplate reactiveMongoTemplate;
 
     private static final Logger log = LoggerFactory.getLogger(SpringBootWebfluxApplication.class);
 
-    public SpringBootWebfluxApplication(ProductoDao productoDao, ReactiveMongoTemplate reactiveMongoTemplate) {
-        this.productoDao = productoDao;
+    public SpringBootWebfluxApplication(ProductoServiceImpl productoService, ReactiveMongoTemplate reactiveMongoTemplate) {
+        this.productoService = productoService;
         this.reactiveMongoTemplate = reactiveMongoTemplate;
     }
 
@@ -34,17 +39,36 @@ public class SpringBootWebfluxApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         reactiveMongoTemplate.dropCollection("productos").subscribe();
+        reactiveMongoTemplate.dropCollection("categorias").subscribe();
 
-        Flux.just(
-                        new Producto("Producto 1", 456.789),
-                        new Producto("Producto 2", 123D),
-                        new Producto("Producto 3", 100D),
-                        new Producto("Producto 4", 199.99),
-                        new Producto("Producto 5", 350.49)
-                ).flatMap(producto -> {
-                    producto.setCreateAt(new Date());
-                    return productoDao.save(producto);
-                })
-                .subscribe(producto -> log.info("Insertado: {} - {}", producto.getId(), producto.getNombre()));
+        Categoria electronica = new Categoria("Electronica");
+        Categoria cocina = new Categoria("Cocina");
+        Categoria muebles = new Categoria("Muebles");
+        Categoria deporte = new Categoria("Deporte");
+
+        Flux.just(electronica, cocina, muebles, deporte)
+                .flatMap(productoService::saveCategoria)
+                .doOnNext(c -> log.info("Categoria creada: {}, Id Cat: {}", c.getNombre(), c.getId()))
+                .thenMany(
+                        Flux.just(
+                                new Producto("Producto 1", 456.789, electronica),
+                                new Producto("Producto 2", 123D, electronica),
+                                new Producto("Producto 3", 100D, muebles),
+                                new Producto("Producto 4", 199.99, cocina),
+                                new Producto("Producto 5", 350.49, deporte)
+                        ).flatMap(producto -> {
+                            producto.setCreateAt(new Date());
+                            return productoService.save(producto);
+                        })
+                )
+                .subscribe(producto -> {
+                            log.info("Producto insertado: {} - {} - {}",
+                                    producto.getId(),
+                                    producto.getNombre(),
+                                    producto.getCategoria().getNombre()
+                            );
+
+                        }
+                );
     }
 }
