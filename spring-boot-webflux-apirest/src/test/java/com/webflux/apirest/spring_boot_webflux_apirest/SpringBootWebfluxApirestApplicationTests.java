e868c8ef4b8cdc9614441c81c53e9ce279b57be9
Carlos Alfonso Chong Zapata <112518890+carloschong10@users.noreply.github.com>
2024-12-24 11:30:17 -0500

    Sección 7: Junit: Test a nuestros EndPoints usando WebTestClient - 79. Test endpoint listar

--- spring-boot-webflux-apirest/src/test/java/com/webflux/apirest/spring_boot_webflux_apirest/SpringBootWebfluxApirestApplicationTests.java
+++ spring-boot-webflux-apirest/src/test/java/com/webflux/apirest/spring_boot_webflux_apirest/SpringBootWebfluxApirestApplicationTests.java
@@ -1,13 +1,28 @@
 package com.webflux.apirest.spring_boot_webflux_apirest;
 
+import com.webflux.apirest.spring_boot_webflux_apirest.models.Producto;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringBootWebfluxApirestApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    void listarTest() {
+        webTestClient.get()
+                .uri("/api/v2/productos")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBodyList(Producto.class)
+                .hasSize(5);
+    }
 
 }
