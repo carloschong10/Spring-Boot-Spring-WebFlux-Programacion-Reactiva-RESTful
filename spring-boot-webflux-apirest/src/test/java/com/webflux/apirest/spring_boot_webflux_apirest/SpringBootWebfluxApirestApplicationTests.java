c3eb6c24672f7e8c3f9f04abff8105ff4cbfac5e
Carlos Alfonso Chong Zapata <112518890+carloschong10@users.noreply.github.com>
2024-12-24 11:38:00 -0500

    80. Test endpoint listar usando consumeWith

--- spring-boot-webflux-apirest/src/test/java/com/webflux/apirest/spring_boot_webflux_apirest/SpringBootWebfluxApirestApplicationTests.java
+++ spring-boot-webflux-apirest/src/test/java/com/webflux/apirest/spring_boot_webflux_apirest/SpringBootWebfluxApirestApplicationTests.java
@@ -1,11 +1,14 @@
 package com.webflux.apirest.spring_boot_webflux_apirest;
 
 import com.webflux.apirest.spring_boot_webflux_apirest.models.Producto;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.List;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringBootWebfluxApirestApplicationTests {
@@ -22,7 +25,13 @@ class SpringBootWebfluxApirestApplicationTests {
                 .expectStatus().isOk()
                 .expectHeader().contentType(MediaType.APPLICATION_JSON)
                 .expectBodyList(Producto.class)
-                .hasSize(5);
+                .consumeWith(response -> {
+                    List<Producto> productos = response.getResponseBody();
+                    productos.forEach(p -> System.out.println(p.getNombre()));
+
+                    Assertions.assertTrue(productos.size() > 0);
+                });
+//                .hasSize(5);
     }
 
 }
