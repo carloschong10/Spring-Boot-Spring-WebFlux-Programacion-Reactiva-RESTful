e3cc4b0355a907f1d1f32ca0d77109f4c343b335
Carlos Alfonso Chong Zapata <112518890+carloschong10@users.noreply.github.com>
2024-12-25 17:05:02 -0500

    Sección 8: WebClient: Consumiendo Servicios RESTful - 88. Creando nuestro microservicio cliente

---
+++ spring-boot-webflux-client/src/test/java/com/webflux/client/spring_boot_webflux_client/SpringBootWebfluxClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webflux.client.spring_boot_webflux_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebfluxClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
