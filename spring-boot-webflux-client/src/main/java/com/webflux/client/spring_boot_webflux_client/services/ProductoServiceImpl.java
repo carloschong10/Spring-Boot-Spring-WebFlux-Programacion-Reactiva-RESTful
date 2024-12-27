46a248785df61ee4f008f71205f758595f79394c
Carlos Alfonso Chong Zapata <112518890+carloschong10@users.noreply.github.com>
2024-12-27 08:50:52 -0500

    91. Implementando el CRUD en el Service

--- spring-boot-webflux-client/src/main/java/com/webflux/client/spring_boot_webflux_client/services/ProductoServiceImpl.java
+++ spring-boot-webflux-client/src/main/java/com/webflux/client/spring_boot_webflux_client/services/ProductoServiceImpl.java
@@ -3,6 +3,8 @@ package com.webflux.client.spring_boot_webflux_client.services;
 import com.webflux.client.spring_boot_webflux_client.models.Producto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -11,6 +13,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+@Service
 public class ProductoServiceImpl implements ProductoService {
 
     @Autowired
@@ -30,27 +33,39 @@ public class ProductoServiceImpl implements ProductoService {
 
         /*
 //        return webClient.get().uri("/{id}", params)
-        return webClient.get().uri("/{id}", Collections.singletonMap("id", 1))
+        return webClient.get().uri("/{id}", Collections.singletonMap("id", id))
                 .accept(MediaType.APPLICATION_JSON)
                 .exchangeToMono(response -> response.bodyToMono(Producto.class));
          */
-        return webClient.get().uri("/{id}", Collections.singletonMap("id", 1))
+        return webClient.get().uri("/{id}", Collections.singletonMap("id", id))
                 .accept(MediaType.APPLICATION_JSON)
                 .retrieve().bodyToMono(Producto.class);
     }
 
     @Override
     public Mono<Producto> save(Producto producto) {
-        return null;
+        return webClient.post()
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(producto))
+                .retrieve().bodyToMono(Producto.class);
     }
 
     @Override
     public Mono<Producto> update(Producto producto, String id) {
-        return null;
+        return webClient.put()
+                .uri("/{id}", Collections.singletonMap("id", id))
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(producto))
+                .retrieve().bodyToMono(Producto.class);
     }
 
     @Override
     public Mono<Void> delete(String id) {
-        return null;
+        return webClient.delete().uri("/{id}", Collections.singletonMap("id", id))
+//                .retrieve().bodyToMono(Void.class);
+                .exchangeToMono(response -> response.bodyToMono(Void.class))
+                .then();
     }
 }
