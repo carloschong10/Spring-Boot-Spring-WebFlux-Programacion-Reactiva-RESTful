de01ac2c6c1fa2ba0f9f5937b251dd5f9afcf487
Carlos Alfonso Chong Zapata <112518890+carloschong10@users.noreply.github.com>
2024-12-28 17:27:03 -0500

    Sección 9: Spring Cloud Eureka Server: Registrando los Microservicios - 101. Creando servidor de registro Eureka

---
+++ spring-boot-webflux-eureka-server/src/test/java/com/webflux/eureka/server/spring_boot_webflux_eureka_server/SpringBootWebfluxEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webflux.eureka.server.spring_boot_webflux_eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebfluxEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
